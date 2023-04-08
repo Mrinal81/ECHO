58e6eefc2a04af812e82b5309b3a87645a6b4ef8
Mrinal Anand <mrinal.anand9123@gmail.com>
2023-04-08 16:38:33 +0530

    completed

---
+++ sanity_echo/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kp3cjqa0',
+    dataset: 'production'
+  }
+})
---
+++ sanity_echo/schemas/schema.js
@@ -0,0 +1,13 @@
+
+
+import product from "./product"
+import banner from "./banner"
+
+
+
+
+
+
+
+
+export const schemaTypes = [product, banner]
